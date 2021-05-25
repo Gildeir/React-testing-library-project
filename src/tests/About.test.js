a760c433ea448da443094f84913244525d251808
Gildeir <gildeirlopes@gmail.com>
2021-05-25 20:45:44 -0300

    Teste se a página contém um heading h2 com o texto About Pokédex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+import About from '../components/About';
+
+describe('teste about', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <About />
+      </Router>,
+    );
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(h2).toBeInTheDocument();
+  });
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {});
+  const history = createMemoryHistory();
+  render(
+    <Router history={ history }>
+      <About />
+    </Router>,
+  );
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
@@ -12,3 +13,31 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+// Teste se a página principal da Pokédex é renderizada ao carregar a aplicação no caminho de URL / .
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+// Teste se o topo da aplicação contém um conjunto fixo de links de navegação.
+// O primeiro link deve possuir o texto Home.
+// O segundo link deve possuir o texto About.
+// O terceiro link deve possuir o texto Favorite Pokémons.
+
+// Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação.
+
+// Teste se a aplicação é redirecionada para a página de About, na URL / about, ao clicar no link About da barra de navegação.
+
+// Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL / favorites, ao clicar no link Favorite Pokémons da barra de navegação.
+
+// Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.
+
+// O que será verificado:
+
+//   Será avaliado se o arquivo teste App.test.js contemplam 100 % dos casos de uso criados pelo Stryker.
