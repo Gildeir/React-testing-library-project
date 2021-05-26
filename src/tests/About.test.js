0240b28d6dd8827f5b11908b5edeea6de1426e74
Gildeir <gildeirlopes@gmail.com>
2021-05-25 21:03:17 -0300

    testa se a pagina contem imagem e se a apagina contem dois paragraphs

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { getAllByAltText, render, screen } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 
@@ -19,11 +19,24 @@ describe('teste about', () => {
     });
     expect(h2).toBeInTheDocument();
   });
-  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {});
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {});
   const history = createMemoryHistory();
   render(
     <Router history={ history }>
       <About />
     </Router>,
   );
+  const paragraphs = getAllByAltText(/Pokémons/i);
+  expect(paragraphs.length).toHaveLength(2);
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {});
+  render(
+    <Router history={ history }>
+      <About />
+    </Router>,
+  );
+  const atribute = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const img = getByRole('img');
+  expect(img).toHaveAttribute('src', atribute);
 });
