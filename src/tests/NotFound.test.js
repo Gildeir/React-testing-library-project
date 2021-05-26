a07a950a9c9ea906097296539bb0dd57cb46dad0
Gildeir <gildeirlopes@gmail.com>
2021-05-26 20:19:18 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from '../components/RenderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('test about', () => {
+  test('Teste se página contém um heading h2 com o texto Page requested not found 😭',
+    () => {
+      RenderWithRouter(<NotFound />);
+      const h2 = screen.getByRole('heading', {
+        level: 2,
+        name: /Page requested not found/i,
+      });
+      expect(h2).toBeInTheDocument();
+    });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    RenderWithRouter(<NotFound />);
+    const atribute = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.queryAllByRole('img');
+    expect(image[1]).toHaveAttribute('src', atribute);
+  });
+});
