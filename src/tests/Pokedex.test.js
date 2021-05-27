210d8f35a20469adcced634eee65fc73d766fbc5
Gildeir <gildeirlopes@gmail.com>
2021-05-27 18:33:05 -0300

    requisito 5 Teste se a Pokédex tem os botões de filtro

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,7 +3,7 @@ import { fireEvent, screen } from '@testing-library/react';
 import RenderWithRouter from '../components/RenderWithRouter';
 import App from '../App';
 
-const pokemonName = 'pokemon-name';
+const pokemonNameId = 'pokemon-name';
 
 describe('test about', () => {
   test('Teste se página contém um heading h2 com o texto Encountered pokémons ',
@@ -19,7 +19,7 @@ describe('test about', () => {
   test('Os próximos Pokémons da lista devem ser mostrados',
     () => {
       RenderWithRouter(<App />);
-      const nextPokemon = screen.getByTestId(pokemonName);
+      const nextPokemon = screen.getByTestId(pokemonNameId);
       const next = screen.getByRole('button', {
         name: 'Próximo pokémon',
       });
@@ -30,7 +30,24 @@ describe('test about', () => {
   test('Teste se é mostrado apenas um pokemon por vez',
     () => {
       RenderWithRouter(<App />);
-      const onePokemon = screen.getAllByTestId(pokemonName);
+      const onePokemon = screen.getAllByTestId(pokemonNameId);
       expect(onePokemon.length).toBe(1);
+    });
+
+  test('Teste se a Pokédex tem os botões de filtro',
+    () => {
+      RenderWithRouter(<App />);
+      const filterButton = screen.getAllByTestId('pokemon-type-button');
+      expect(filterButton).toBeDefined();
+    });
+
+  test('Teste se a Pokédex tem os botões de filtro',
+    () => {
+      const { getByRole } = RenderWithRouter(<App />);
+      const endFilter = getByRole('button', {
+        name: 'All',
+      });
+      // fireEvent.click(endFilter);
+      expect(endFilter).toBeInTheDocument();
     });
 });
