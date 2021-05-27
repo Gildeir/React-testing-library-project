6ed49d8307645e83b6b630eafe4cd0203d5760f7
Gildeir <gildeirlopes@gmail.com>
2021-05-27 15:34:27 -0300

    Iniciando requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from '../components/RenderWithRouter';
+import App from '../App';
+
+describe('test about', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons ',
+    () => {
+      RenderWithRouter(<App />);
+      const h2 = screen.getByRole('heading', {
+        level: 2,
+        name: 'Encountered pokémons',
+      });
+      expect(h2).toBeInTheDocument();
+    });
+
+  // test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  //   RenderWithRouter(<Pokedex />);
+  //   const atribute = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  //   const image = screen.queryAllByRole('img');
+  //   expect(image[1]).toHaveAttribute('src', atribute);
+  // });
+});
