61001c2c2a763e28c7f16b5a4dd693aad808263b
Gildeir <gildeirlopes@gmail.com>
2021-05-28 01:19:10 -0300

    Adicionando funçao para auxiliar no lint

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
+import { fireEvent } from '@testing-library/dom';
 import RenderWithRouter from '../components/RenderWithRouter';
 import App from '../App';
 
 const detailsOfPokemon = 'Pikachu Details';
-const moreDetails = 'More details';
+
+const clickMoreDetails = () => {
+  const { getByText } = RenderWithRouter(<App />);
+  const getMoreDetails = getByText('More details');
+  expect(getMoreDetails).toBeInTheDocument();
+  fireEvent.click(getMoreDetails);
+};
 
 describe('Teste se as informações detalhadas do'
 + 'Pokémon selecionado são mostradas na tela.', () => {
   test('A página deve conter um texto <name> Details, onde <name> é o nome do Pokémon ',
     () => {
+      clickMoreDetails();
       const { getByText, getByRole } = RenderWithRouter(<App />);
-      const getMoreDetails = getByText(moreDetails);
-      userEvent.click(getMoreDetails);
       const pokemonDetails = getByText(detailsOfPokemon);
       expect(pokemonDetails).toBeInTheDocument();
-      expect(getMoreDetails).not.toBeInTheDocument();
+      // expect(getMoreDetails).not.toBeInTheDocument();
 
       const heading = getByRole('heading', {
         level: 2,
@@ -25,23 +31,28 @@ describe('Teste se as informações detalhadas do'
       const text = getByText(/This intelligent Pokémon roasts hard berries/i);
       expect(text).toBeInTheDocument();
     });
-
+  test('', () => {
+    clickMoreDetails();
+    const { getByText } = RenderWithRouter(<App />);
+    const message = 'Game Locations of Pikachu';
+    const text = getByText(message);
+    expect(text).toBeInTheDocument();
+  });
   test('Teste se existe na página uma seção'
   + 'com os mapas contendo as localizações do pokémon', () => {
+    clickMoreDetails();
     const { getAllByRole } = RenderWithRouter(<App />);
     const image = getAllByRole('img');
 
-    expect(image[0]).toBeInTheDocument();
-    expect(image[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
-    expect(image[0]).toHaveAttribute('alt', 'Pikachu sprite');
+    expect(image[1]).toBeInTheDocument();
+    expect(image[1]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(image[1]).toHaveAttribute('alt', 'Pikachu location');
   });
 
   test('Teste se o usuário pode favoritar um pokémon'
   + 'através da página de detalhes', () => {
-    const { getByRole, getByText, getByLabelText } = RenderWithRouter(<App />);
-    const getMoreDetails = getByText('More details');
-    userEvent.click(getMoreDetails);
-
+    clickMoreDetails();
+    const { getByRole, getByLabelText } = RenderWithRouter(<App />);
     const check = getByRole('checkbox');
     expect(check).toBeInTheDocument();
     userEvent.click(check);
